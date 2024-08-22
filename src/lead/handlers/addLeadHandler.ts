120f76bde709936546da734169a8ab448e4c2b56
chandrama-crowdapps <91244121+chandrama-crowdapps@users.noreply.github.com>
2024-08-22 18:43:31 +0530

    code updated

--- src/lead/handlers/addLeadHandler.ts
+++ src/lead/handlers/addLeadHandler.ts
@@ -17,7 +17,7 @@ export const addLeadHandler: RouteHandler = async (
         const user = (event.requestContext as any).user;
         logger.info('user:', { user });
 
-        const hasPermission = await checkPermission(user.role, 'staff', 'create', tenant.schema);
+        const hasPermission = await checkPermission(user.role, 'Lead', 'create', tenant.schema);
         logger.info('hasPermission: -----------', { hasPermission });
         if (!hasPermission) {
             return ResponseHandler.forbiddenResponse({ message: 'Permission denied' });
